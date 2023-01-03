141c36ff5581cbaf0bbc1afca4ae75af70ebb41b
ankitbhusal <ankitbhusal95@gmail.com>
2023-01-03 21:28:59 +0545

    backend for chat app

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const chats = require("./data/data");
+
+const dotenv = require("dotenv");
+
+const cors = require("cors");
+
+const app = express();
+dotenv.config();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("Api is running");
+});
+
+
+
+app.get("/api/chat", (req, res) => {
+    res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+    // console.log(req.params.id)
+    const singleChat = chats.find((c) => c._id === req.params.id);
+    res.send(singleChat);
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server started on port: ${process.env.PORT}`);
+});
